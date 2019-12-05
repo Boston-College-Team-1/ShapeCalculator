ee274961f41798e86516aff42a17e5a848748013
ShadowCoder7274 <58554055+LukeSmall@users.noreply.github.com>
2019-12-05 14:27:56 +0000

    Added Rectangle

--- ShapeCalulator/Rectangle.cs
+++ ShapeCalulator/Rectangle.cs
@@ -4,11 +4,33 @@ using System.Text;
 
 namespace ShapeCalulator
 {
-    class Rectangle
+    class Rectangle: VertexBasedShape2D
     {
         public override void Run()
         {
+            length = InputOutput.CollectDouble("Please input length");
+            width = InputOutput.CollectDouble("Please input width");
+            CalcualteArea();
+            CalculatePerimeter();
+            OutputResaults();
 
         }
+        private void CalcualteArea()
+        {
+            area = width * length;
+        }
+
+        private void CalculatePerimeter()
+        {
+            perimeter = 2 * (length + width);
+        }
+
+        private void OutputResaults()
+        {
+            Console.WriteLine("The area of the Rectangle is: " + area);
+            Console.WriteLine("The periemter of the Rectangle is: " + perimeter);
+        }
+
+
     }
 }
