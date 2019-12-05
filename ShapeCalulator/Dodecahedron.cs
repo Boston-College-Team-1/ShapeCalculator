68986165a1008783cf48e14f293b385938720734
apple <43368992+HardijsR@users.noreply.github.com>
2019-12-05 14:14:07 +0000

    Dodecahedron Shape

---
+++ ShapeCalulator/Dodecahedron.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShapeCalulator
+{
+    class Dodecahedron: VertexBasedShape3D
+    {
+        public override void Run()
+        {
+            Input();
+            CalculateSurfaceArea();
+            CalculateVolume();
+            Output();
+        }
+        void Input()
+        {
+            length = InputOutput.CollectDouble("Edge Length");
+        }
+        void CalculateSurfaceArea() => surfaceArea = 3 * Math.Sqrt(25 + 10 * Math.Sqrt(5)) * Math.Pow(length, 2);
+        void CalculateVolume() => volume = (15 + 7 * Math.Sqrt(5) / 4) * Math.Pow(length, 3);
+        void Output() 
+        {
+            InputOutput.OutputDouble("Volume", volume);
+            InputOutput.OutputDouble("Surface Area", surfaceArea);
+        }
+
+    }
+}
