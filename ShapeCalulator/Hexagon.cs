14aea54d9fcce53c74c2fc37e0a0b75803fad929
apple <43368992+HardijsR@users.noreply.github.com>
2019-12-05 14:33:58 +0000

    Hexagon, Pentagon, Dodecahedron

---
+++ ShapeCalulator/Hexagon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShapeCalulator
+{
+    class Hexagon : VertexBasedShape2D
+    {
+        public override void Run()
+        {
+            Input();
+            CalculateArea();
+            CalculatePerimeter();
+            Output();
+        }
+        void Input()
+        {
+            length = InputOutput.CollectDouble("Edge Length");
+        }
+        void CalculateArea() => area = ((3 * Math.Sqrt(3)) / 4) * Math.Pow(length, 2);
+        void CalculatePerimeter() => perimeter = 6 * length;
+        void Output()
+        {
+            InputOutput.OutputDouble("Area", area);
+            InputOutput.OutputDouble("Perimeter", perimeter);
+        }
+    }
+}
