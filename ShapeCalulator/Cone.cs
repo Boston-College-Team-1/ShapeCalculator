8a63af8d273c38a46c94372fbf352e3414af6c11
Cwelton1 <58041303+Cwelton1@users.noreply.github.com>
2019-12-05 14:21:59 +0000

    Added cone

---
+++ ShapeCalulator/Cone.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShapeCalulator
+{
+    class Cone : NonVertexBasedShape3D
+    {
+        private double height = 0.0;
+
+        private void CalculateSA() //Surface Area
+        {
+            surfaceArea = Math.PI * radius * (radius + Math.Sqrt(Math.Pow(height, 2) + Math.Pow(radius, 2)));
+        }
+        private void CalculateVolume() //Surface Area
+        {
+            volume = Math.PI * Math.Pow(radius, 2) * height / 3;
+        }
+        public override void Run()
+        {
+            radius = InputOutput.CollectDouble("Please enter Radius of cone");
+            height = InputOutput.CollectDouble("Please enter height of cone");
+            CalculateSA();
+            Console.WriteLine($"the surface area is {surfaceArea}");
+            CalculateVolume();
+            Console.WriteLine($"the Volume is {volume}");
+        }
+    }
+}
