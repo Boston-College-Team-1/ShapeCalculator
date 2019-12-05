9aa0a72de51ce1620625857a9164a347c3874a05
Cwelton1 <58041303+Cwelton1@users.noreply.github.com>
2019-12-05 14:47:43 +0000

    Added the triangles

---
+++ ShapeCalulator/Equilateral.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShapeCalulator
+{
+    class Equilateral: VertexBasedShape2D
+    {
+        private void CalculateArea()
+        {
+            area = (Math.Sqrt(3.0) / 4.0) * Math.Pow(length, 2.0);
+        }
+        private void CalculatePerimeter()
+        {
+            perimeter = 3.0 * length;
+        }
+
+        public override void Run()
+        {
+            length = InputOutput.CollectDouble("Please enter base of Cuboid");
+            width = InputOutput.CollectDouble("Please enter side length of Cuboid");
+            height = InputOutput.CollectDouble("Please enter height of Cuboid");
+            CalculateArea();
+            Console.WriteLine($"the surface area is {area}");
+            CalculatePerimeter();
+            Console.WriteLine($"the Volume is {perimeter}");
+        }
+    }
+}
---
+++ ShapeCalulator/Isosceles.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShapeCalulator
+{
+    class Isosceles : VertexBasedShape2D
+    {
+        private void CalculateArea()
+        {
+            area = (height * length) / 2;
+        }
+        private void CalculatePerimeter()
+        {
+            perimeter = height + length + width;
+        }
+        public override void Run()
+        {
+            length = InputOutput.CollectDouble("Please enter base of Cuboid");
+            width = InputOutput.CollectDouble("Please enter side length of Cuboid");
+            height = InputOutput.CollectDouble("Please enter height of Cuboid");
+            CalculateArea();
+            Console.WriteLine($"the surface area is {area}");
+            CalculatePerimeter();
+            Console.WriteLine($"the Volume is {perimeter}");
+        }
+    }
+}
